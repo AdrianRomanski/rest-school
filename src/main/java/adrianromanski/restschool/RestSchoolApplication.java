bcc57e85874fbe2d08bd5ade764ff5b23f8997e0
Adrian Romański <muszynianka1337@gmail.com>
2020-03-24 19:32:24 +0100

    Initial commit

---
+++ src/main/java/adrianromanski/restschool/RestSchoolApplication.java
@@ -0,0 +1,13 @@
+package adrianromanski.restschool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestSchoolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestSchoolApplication.class, args);
+    }
+
+}
---
+++ src/test/java/adrianromanski/restschool/RestSchoolApplicationTests.java
@@ -0,0 +1,13 @@
+package adrianromanski.restschool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestSchoolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

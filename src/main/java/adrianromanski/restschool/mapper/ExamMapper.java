7d08df97f4fa35d16340e598e3303b018e03899d
Adrian Romański <muszynianka1337@gmail.com>
2020-03-28 18:47:41 +0100

    Added Mapper for Exam

---
+++ src/main/java/adrianromanski/restschool/mapper/ExamMapper.java
@@ -0,0 +1,31 @@
+package adrianromanski.restschool.mapper;
+
+import adrianromanski.restschool.domain.Exam;
+import adrianromanski.restschool.model.ExamDTO;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface ExamMapper {
+
+    ExamMapper INSTANCE = Mappers.getMapper(ExamMapper.class);
+
+
+    @Mappings({
+            @Mapping(source = "subject", target = "subjectDTO"),
+            @Mapping(source = "students", target = "studentsDTO")
+    })
+    ExamDTO ExamToExamDTO(Exam exam);
+
+
+    @Mappings({
+            @Mapping(source = "subjectDTO", target = "subject"),
+            @Mapping(source = "studentsDTO", target = "students")
+    })
+    ExamDTO ExamDTOToExam(ExamDTO examDTO);
+
+
+
+}

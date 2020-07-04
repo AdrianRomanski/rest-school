225607931028e7fc0bbb2e7fce68095adfbea538
adrianromanski <adrianromanski@icloud.com>
2020-07-04 11:50:06 +0200

    Further Testing on SchoolYear Entity Mappings

--- src/test/java/adrianromanski/restschool/mapper/event/SchoolYearMapperTest.java
+++ src/test/java/adrianromanski/restschool/mapper/event/SchoolYearMapperTest.java
@@ -3,11 +3,13 @@ package adrianromanski.restschool.mapper.event;
 import adrianromanski.restschool.domain.enums.Sport;
 import adrianromanski.restschool.domain.event.SchoolYear;
 import adrianromanski.restschool.domain.group.SportTeam;
+import adrianromanski.restschool.domain.group.StudentClass;
 import adrianromanski.restschool.domain.group.TeachingStaff;
 import adrianromanski.restschool.domain.person.Director;
 import adrianromanski.restschool.domain.person.Teacher;
 import adrianromanski.restschool.model.event.SchoolYearDTO;
 import adrianromanski.restschool.model.group.SportTeamDTO;
+import adrianromanski.restschool.model.group.StudentClassDTO;
 import adrianromanski.restschool.model.group.TeachingStaffDTO;
 import adrianromanski.restschool.model.person.DirectorDTO;
 import adrianromanski.restschool.model.person.TeacherDTO;
@@ -35,12 +37,24 @@ class SchoolYearMapperTest {
         SchoolYear schoolYear = SchoolYear.builder().name(NAME).date(LOCAL_DATE).build();
 
         SportTeam sportTeam = SportTeam.builder().sport(FOOTBALL).name(SULTANS).president(MAHARAJAH).build();
+        sportTeam.setSchoolYear(schoolYear);
+        schoolYear.setSportTeams(Collections.singletonList(sportTeam));
+
         Director director = Director.builder().firstName(FIRST_NAME).lastName(LAST_NAME).build();
+        director.getSchoolYears().add(schoolYear);
+        schoolYear.setDirector(director);
+
         TeachingStaff teachingStaff = new TeachingStaff();
         teachingStaff.setTeachers(Arrays.asList(new Teacher(), new Teacher()));
-        schoolYear.setDirector(director);
-        schoolYear.setSportTeams(Collections.singletonList(sportTeam));
+        teachingStaff.setSchoolYear(schoolYear);
         schoolYear.setTeachingStaff(teachingStaff);
+
+        StudentClass studentClass = StudentClass.builder().name("Biology").build();
+        StudentClass studentClass1 = StudentClass.builder().name("Math").build();
+        studentClass.setSchoolYear(schoolYear);
+        studentClass1.setSchoolYear(schoolYear);
+        schoolYear.setStudentClasses(Arrays.asList(studentClass, studentClass1));
+
 
         SchoolYearDTO schoolYearDTO = mapper.schoolYearToSchoolYearDTO(schoolYear);
 
@@ -54,6 +68,8 @@ class SchoolYearMapperTest {
         assertEquals(schoolYearDTO.getSportTeamsDTO().get(0).getName(), SULTANS);
         assertEquals(schoolYearDTO.getSportTeamsDTO().get(0).getPresident(), MAHARAJAH);
 
+        assertEquals(schoolYearDTO.getStudentClassesDTO().size(), 2);
+
     }
 
     @Test
@@ -61,12 +77,24 @@ class SchoolYearMapperTest {
         SchoolYearDTO schoolYearDTO = SchoolYearDTO.builder().name(NAME).date(LOCAL_DATE).build();
 
         SportTeamDTO sportTeamDTO = SportTeamDTO.builder().sport(FOOTBALL).name(SULTANS).president(MAHARAJAH).build();
+        sportTeamDTO.setSchoolYearDTO(schoolYearDTO);
+        schoolYearDTO.setSportTeamsDTO(Collections.singletonList(sportTeamDTO));
+
         DirectorDTO directorDTO = DirectorDTO.builder().firstName(FIRST_NAME).lastName(LAST_NAME).build();
+        directorDTO.getSchoolYearsDTO().add(schoolYearDTO);
+        schoolYearDTO.setDirectorDTO(directorDTO);
+
         TeachingStaffDTO teachingStaffDTO = new TeachingStaffDTO();
         teachingStaffDTO.setTeachersDTO(Arrays.asList(new TeacherDTO(), new TeacherDTO()));
-        schoolYearDTO.setDirectorDTO(directorDTO);
-        schoolYearDTO.setSportTeamsDTO(Collections.singletonList(sportTeamDTO));
+        teachingStaffDTO.setSchoolYearDTO(schoolYearDTO);
         schoolYearDTO.setTeachingStaffDTO(teachingStaffDTO);
+
+
+        StudentClassDTO studentClassDTO = StudentClassDTO.builder().name("Biology").build();
+        StudentClassDTO studentClassDTO1 = StudentClassDTO.builder().name("Math").build();
+        studentClassDTO.setSchoolYearDTO(schoolYearDTO);
+        studentClassDTO1.setSchoolYearDTO(schoolYearDTO);
+        schoolYearDTO.setStudentClassesDTO(Arrays.asList(studentClassDTO, studentClassDTO1));
 
         SchoolYear schoolYear = mapper.schoolYearDTOToSchoolYear(schoolYearDTO);
 
@@ -79,5 +107,7 @@ class SchoolYearMapperTest {
         assertEquals(schoolYear.getSportTeams().get(0).getSport(), FOOTBALL);
         assertEquals(schoolYear.getSportTeams().get(0).getName(), SULTANS);
         assertEquals(schoolYear.getSportTeams().get(0).getPresident(), MAHARAJAH);
+
+        assertEquals(schoolYear.getStudentClasses().size(), 2);
     }
 }

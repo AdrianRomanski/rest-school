26f4d6bec20f62ac59df6cd95cbf2dd4ca4a6420
adrianromanski <adrianromanski@icloud.com>
2020-07-13 11:07:18 +0200

    Fixing swagger related broken build

--- src/main/java/adrianromanski/restschool/config/SwaggerConfig.java
+++ src/main/java/adrianromanski/restschool/config/SwaggerConfig.java
@@ -2,13 +2,11 @@ package adrianromanski.restschool.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.data.rest.configuration.SpringDataRestConfiguration;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
@@ -17,7 +15,6 @@ import java.util.Collections;
 
 @Configuration
 @EnableSwagger2
-@Import(SpringDataRestConfiguration.class)
 public class SwaggerConfig {
 
     @Bean
